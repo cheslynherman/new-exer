ac7bc76be39482b3443d217175c6bd8402998e2d
Cheslyn Herman <cheslynherman1169@gmail.com>
2023-06-02 13:43:52 +0200

    arrays exercise

--- index.js
+++ index.js
@@ -3,18 +3,39 @@ function addToArray(){
     let input = document.getElementById("array").value
     person.push (input)
     document.getElementById("newArray").innerHTML= person
+
+    
+    console.log()
 }
 
 let people= []
 function addToObject(){
-    let name = document.getElementById("firstName").value
-    let surname = document.getElementById("lastName").value
-    people.push (name, surname)
-    document.getElementById("result").innerHTML= people
+    // let name = document.getElementById("firstName").value
+    // let surname = document.getElementById("lastName").value
+    // let age = document.getElementById("ages").value
+    // people.push (name, surname, age)
+
+    let person = {
+        name: document.getElementById("firstName").value, 
+        surname: document.getElementById("lastName").value,
+        age: document.getElementById("ages").value
+    }
+    people.push(person);
+    
+    let table = document.querySelector(".table");
+    table.innerHTML= "";
+    people.forEach((data)=> {
+        table.innerHTML +=
+        `
+        <tbody>
+        <tr>
+          <td>${data.name}</td>
+          <td>${data.surname}</td>
+          <td>${data.age}</td><br>
+        </tr>
+        </tbody>`
+
+        localStorage.setItem("name", JSON.stringify(people));
+    })
 }
 
-// let person= [
-//     {name: "Grant"},
-//     {name: "Nigel"},
-//     {name: "Henry"}
-// ]
